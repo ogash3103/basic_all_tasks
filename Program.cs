0671717f6c97162db2e2aa9d29f4dbcc30147e6f
ogash3103 <faxogabek@gmail.com>
2024-12-26 21:06:17 +0500

    i have done three tasks

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+
+//=================  task 1 start
+
 System.Console.Write("Radiusni qiymatini kiriting: ");
        decimal radius = Convert.ToDecimal(Console.ReadLine());
 
@@ -11,4 +14,45 @@ System.Console.Write("Radiusni qiymatini kiriting: ");
         System.Console.WriteLine($"Doiraning yuzasi: {S}");
         System.Console.WriteLine($"Doiraning uzunligi: {L}");
 
-        
+// task 1 end
+
+
+// task 2 start
+
+System.Console.Write("Qiymat kiriting: ");
+double qiymat = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Kurs qiyimatini kiriting (so'm): ");
+double kurs = Convert.ToDouble(Console.ReadLine());
+
+// hisob kitobda decimal ishlatish kk
+decimal summa = (decimal)(qiymat * kurs);
+
+System.Console.WriteLine($"natija: {summa} so'm");
+
+// ============= task 2 end
+
+
+
+// task 3 start
+
+// inoput orqali kirgizishlar
+System.Console.Write("Tug'ulgan yillingizni kirgizing: ");
+int bornYear = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Hozirgi yilni kirgizing: ");
+int currentYear = Convert.ToInt32(Console.ReadLine());
+
+// hisoblash
+int yillar = currentYear - bornYear;
+int kunlar = yillar * 365;
+
+System.Console.WriteLine($"Hozirgi yilgacha: {kunlar} kun utdi");
+
+// task 3 end
+
+
+
+
+
+
+// task 2 end
--- obj/Debug/net8.0/basic_all_tasks.AssemblyInfo.cs
+++ obj/Debug/net8.0/basic_all_tasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basic_all_tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05c3f489d1842b6f53a357f01c8d1a05adda123")]
 [assembly: System.Reflection.AssemblyProductAttribute("basic_all_tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basic_all_tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
