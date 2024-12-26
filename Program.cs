c05c3f489d1842b6f53a357f01c8d1a05adda123
ogash3103 <faxogabek@gmail.com>
2024-12-26 17:16:58 +0500

    I have done first task

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+System.Console.Write("Radiusni qiymatini kiriting: ");
+       decimal radius = Convert.ToDecimal(Console.ReadLine());
+
+        decimal pi = (decimal)Math.PI;
+
+        decimal S = pi * (decimal)(radius * radius); 
+        decimal L = 2 * pi * (decimal)radius; 
+
+        System.Console.WriteLine($"Doiraning yuzasi: {S}");
+        System.Console.WriteLine($"Doiraning uzunligi: {L}");
+
+        
